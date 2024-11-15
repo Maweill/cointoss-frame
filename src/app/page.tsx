8580c706e86f76a39e139b507e582b88099b86da
bednyakov <bednyakov99@gmail.com>
2024-11-15 11:03:32 +0300

    fix: url usage from env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export async function generateMetadata() {
         new URL(
           "/frames",
           process.env.VERCEL_URL
-            ? `https://${process.env.VERCEL_URL}`
+            ? process.env.VERCEL_URL
             : "http://localhost:3000"
         )
       )),
